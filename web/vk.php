c513986709ee890f6a41b8171e630e7302310378
Staschik <stas@biche-ool.ru>
2020-02-06 16:21:36 +0700

    vk bot test

--- web/vk.php
+++ web/vk.php
@@ -1,6 +1,26 @@
 <?php
 
-$message = json_decode(file_get_contents('php://input'), true);
-if ($message['type'] == 'confirmation' && $message['group_id'] == 191703401) {
-    return '7a619544';
-}
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Telegram\TelegramDriver;
+use Bot\Config;
+use Bot\DBConnect;
+use BotMan\Drivers\Vk\VkDriver;
+
+
+$appConfig = json_decode(file_get_contents(__DIR__ . '/../config.json'), true);
+$db = DBConnect::connect($appConfig['database']);
+$config = [
+    // Your driver-specific configuration
+//    "telegram" => $appConfig['telegram']
+    "access_token" => '0e9adee36161f6e653cf8a2a659cde42b42f360da4bf3a291128fa850d2743cdc96cf3af418f857e0f2b3',
+    "api_version" => '5.50'
+];
+DriverManager::loadDriver(VkDriver::class);
+$botman = BotManFactory::create($config);
+
+$botman->hears('/start', function (BotMan $bot) {
+    $bot->reply('Tested');
+});
+$botman->listen();
