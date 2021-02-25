741b55a10fd110808cc39cce00aa72a91541262f
Stanislav B <stas@biche-ool.ru>
2021-02-25 11:43:47 +0700

    worker.php теперь работает постоянно

--- src/worker.php
+++ src/worker.php
@@ -8,15 +8,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 $db = DB::connect($_ENV['DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
-$task = $db->getNewTask();
-if (is_null($task)) {
-    sleep(10);
-    exit(0);
+while (true) {
+    $task = $db->getNewTask();
+    if (is_null($task)) {
+        sleep(10);
+        continue;
+    }
+
+
+    $font = __DIR__ . '/../fonts/NotoSans-Regular.ttf';
+    $gifFilePath = (new Gifer($font, $task))->process();
+    Gifer::sendGif($task['user_id'], $gifFilePath, $_ENV['TELEGRAM_TOKEN'], $_ENV['TELEGRAM_PROXY'] ?? null);
+    unlink($gifFilePath);
+    $db->setTaskDone($task['id']);
 }
-
-
-$font = __DIR__ . '/../fonts/NotoSans-Regular.ttf';
-$gifFilePath = (new Gifer($font, $task))->process();
-Gifer::sendGif($task['user_id'], $gifFilePath, $_ENV['TELEGRAM_TOKEN'], $_ENV['TELEGRAM_PROXY'] ?? null);
-unlink($gifFilePath);
-$db->setTaskDone($task['id']);
