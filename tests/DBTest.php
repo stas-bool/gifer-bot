e34b3938cf18807599a8a579c9db01907e420cec
Stanislav B <stas@biche-ool.ru>
2021-02-26 15:55:11 +0700

    Fix DBTest.php

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -7,31 +7,48 @@ namespace Test;
 use Bot\Config;
 use Bot\DB;
 use PHPUnit\Framework\TestCase;
+use RuntimeException;
 
 class DBTest extends TestCase
 {
     private static $userId = 132763295;
     protected static $db;
 
-    public function testConnect(): void
+    public static function setUpBeforeClass(): void
     {
         self::$db = DB::connect($_ENV['DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        self::$db->query("DELETE FROM task");
+        self::$db->query("DELETE FROM user_config");
+    }
+
+    public function testConnect(): void
+    {
         self::assertInstanceOf(DB::class, self::$db);
     }
 
+    /**
+     * @depends testConnect
+     */
     public function testNewUserConfig(): void
     {
         $dbConfig = self::$db->getConfigByUserId(self::$userId);
+        self::assertFalse($dbConfig);
+
         $config = Config::get(self::$userId, $dbConfig);
         self::assertTrue(self::$db->saveConfig($config), 'Не удалось создать конфигурацию пользователя');
     }
 
+    /**
+     * @depends testConnect
+     * @depends testNewUserConfig
+     * @return int
+     */
     public function testNewTask(): int
     {
         $configFromDb = self::$db->getConfigByUserId(self::$userId);
         $userConfig = Config::get(self::$userId, $configFromDb);
         $newTaskId = self::$db->newTask($userConfig->getUserId(), 'test text');
-        self::assertIsInt($newTaskId);
+        self::assertIsInt($newTaskId, "Не удалось создать задание");
         return $newTaskId;
     }
 
@@ -43,7 +60,7 @@ class DBTest extends TestCase
     public function testGetTask(int $taskId): int
     {
         $task = self::$db->getNewTask();
-        self::assertIsArray($task);
+        self::assertIsArray($task, "Не удалось получить новое задание");
         self::assertEquals($taskId, $task['id']);
         return $task['id'];
     }
@@ -55,9 +72,13 @@ class DBTest extends TestCase
     public function testSetTaskDone(int $taskId): void
     {
         self::$db->setTaskDone($taskId);
-        self::assertNull(self::$db->getNewTask());
+        self::assertNull(self::$db->getNewTask(), "Не удалось отметить задание выполненным");
     }
 
+    /**
+     * @depends testConnect
+     * @depends testNewUserConfig
+     */
     public function testSaveWrongData(): void
     {
         $config = Config::get(self::$userId, self::$db->getConfigByUserId(self::$userId));
@@ -67,13 +88,17 @@ class DBTest extends TestCase
         self::assertFalse(self::$db->saveConfig($config));
     }
 
+    /**
+     * @depends testConnect
+     * @depends testNewUserConfig
+     */
     public function testTooLongText(): void
     {
         $text = $this->generateRandomString();
         $configFromDb = self::$db->getConfigByUserId(self::$userId);
         $userConfig = Config::get(self::$userId, $configFromDb);
 
-        $this->expectException(\RuntimeException::class);
+        $this->expectException(RuntimeException::class);
         self::$db->newTask($userConfig->getUserId(), $text);
     }
 
