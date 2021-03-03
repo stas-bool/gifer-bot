e31bdb723be6d51bf5f0dd84b876452daa6d9ba9
Stanislav B <stas@biche-ool.ru>
2021-03-03 12:28:37 +0700

    worker new DB api

--- src/worker.php
+++ src/worker.php
@@ -1,44 +1,50 @@
 <?php
 
-use Gifer\Gifer;
 use Bot\DB;
+use Bot\model\Config;
+use Bot\model\Task;
+use Bot\Registry;
+use Gifer\Gifer;
 use Bot\TelegramClient;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../config/');
 $dotenv->load();
-$db = DB::connect($_ENV['DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+$registry = Registry::getInstance();
+$registry->pdo = new PDO($_ENV['DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+DB::createTables($registry->pdo);
 while (true) {
-    $task = $db->getNewTask();
+    $transaction = $registry->pdo->beginTransaction();
+    $task = Task::find()->where(['status' => Task::STATUS_NEW])->forUpdate()->one();
     if (is_null($task)) {
+        $registry->pdo->rollBack();
         sleep(10);
         continue;
     }
 
-
     $font = __DIR__ . '/../fonts/NotoSans-Regular.ttf';
 
-    $gifFile = sys_get_temp_dir()."/{$task['user_id']}.gif";
+    $gifFile = sys_get_temp_dir()."/{$task->getId()}.gif";
+    $config = Config::find()->byId($task->getConfig());
     if (!file_exists($gifFile)) {
-        // TODO Если файл существует, то не надо генерировать gif
+        $gif = Gifer::createGif($font, $task->getText(), $config->getBgColor(), $config->getFontColor(), $config->getSpeed());
+        file_put_contents($gifFile, $gif);
     }
-    $gif = Gifer::createGif($font, $task['text'], $task['bg_color'], $task['font_color'], $task['speed']);
-
-    file_put_contents($gifFile, $gif);
 
     $success = TelegramClient::sendGif(
-        $task['user_id'],
+        $config->getId(),
         $gifFile,
         $_ENV['TELEGRAM_TOKEN'],
         $_ENV['TELEGRAM_PROXY'] ?? null
     );
 
-    unlink($gifFile);
     if ($success) {
-        $db->setTaskDone($task['id']);
+        unlink($gifFile);
+        $task->setStatus(Task::STATUS_DONE)->update();
+        $registry->pdo->commit();
     } else {
-        // TODO Если не удалось выполнить, вернуть статус new
+        $registry->pdo->rollBack();
         sleep(10);
     }
 }
